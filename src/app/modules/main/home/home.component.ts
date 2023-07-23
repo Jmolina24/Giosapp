ca17e0135cab3a6b558e5454ae2f0c4c6cb2523b
Isaias Palacio <106626768+palacioia0312@users.noreply.github.com>
2023-07-23 10:23:20 -0500

    Home: Ya filtra por idcliente, idclientesede y/o idtercero: Finalizado la parte de graficas

--- src/app/modules/main/home/home.component.ts
+++ src/app/modules/main/home/home.component.ts
@@ -28,67 +28,12 @@ export type ChartOptions = {
 	xaxis: ApexXAxis;
 };
 
-const githubIssues = {
-	overview: {
-		'this-week': {
-			'new-issues': 214,
-			'closed-issues': 75,
-			fixed: 3,
-			'wont-fix': 4,
-			're-opened': 8,
-			'needs-triage': 6,
-		},
-		'last-week': {
-			'new-issues': 197,
-			'closed-issues': 72,
-			fixed: 6,
-			'wont-fix': 11,
-			're-opened': 6,
-			'needs-triage': 5,
-		},
-	},
-	labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-	series: {
-		'this-week': [
-			{
-				name: 'New issues',
-				type: 'line',
-				data: [42, 28],
-			},
-			{
-				name: 'Closed issues',
-				type: 'bar',
-				data: [11, 10],
-			},
-		],
-		'last-week': [
-			{
-				name: 'New issues',
-				type: 'line',
-				data: [37, 32, 39, 27, 18, 24, 20],
-			},
-			{
-				name: 'Closed issues',
-				type: 'column',
-				data: [9, 8, 10, 12, 7, 11, 15],
-			},
-			{
-				name: 'Closed issues',
-				type: 'column',
-				data: [9, 8, 10, 12, 7, 11, 15],
-			},
-		],
-	},
-};
-
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-	// @ViewChild('chart') chart: ChartComponent;
-
 	chartsResumenMes: ApexOptions;
 	chartsProgressServices: Partial<ChartOptions>;
 
@@ -101,14 +46,14 @@ export class HomeComponent implements OnInit, AfterViewInit {
 	};
 
 	constructor(
-		private _orders: OrdersService,
 		private _storage: StorageService,
 		private _score: ScoreService
 	) {
 		this.user = _storage.getUser();
 	}
 	ngAfterViewInit(): void {
-		this._score.get().subscribe((r) => {
+		const { idcliente,  idclientesede, idtercero } = this.user;
+		this._score.get({ idcliente,  idclientesede, idtercero }).subscribe((r) => {
 			this.score = r;
 			this._prepareChartData(r.resumen_mes);
 			this._loadDataChart(r.progreso_servicio);
@@ -142,7 +87,6 @@ export class HomeComponent implements OnInit, AfterViewInit {
 	}
 
 	private _prepareChartData(data: any[]): void {
-		console.log(data);
 		this.chartsResumenMes = {
 			chart: {
 				fontFamily: 'inherit',
